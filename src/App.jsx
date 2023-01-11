b2c6988023a1b2c3100363f697e660b6bce24be5
carmine_dyj <dyujie@outlook.com>
2023-01-11 22:40:40 +0800

    feat(layout): complete main layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
 function App() {
-  return <main></main>
+  return (
+    <>
+      <h1 class="text-title text-center text-18px font-extrabold center sm:mt-60px md:mt-100px sm:mb-18px md:mb-28px">
+        Solid Password Generator
+      </h1>
+      <main class="main-bg-color center sm:w-85vw md:w-600px">
+        <div class="h-100px"></div>
+        <div class="index-bg-color h-30px"></div>
+        <div class="h-400px"></div>
+      </main>
+    </>
+  )
 }
 
 export default App
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,26 @@ import solidPlugin from 'vite-plugin-solid'
 import Unocss from 'unocss/vite'
 
 export default defineConfig({
-  plugins: [solidPlugin(), Unocss({ rules: [['index-bg-color', { 'background-color': '#1a2a3a' }]] })],
+  plugins: [
+    solidPlugin(),
+    Unocss({
+      rules: [
+        ['index-bg-color', { 'background-color': '#1a2a3a' }],
+        ['main-bg-color', { 'background-color': '#4a5a6a' }],
+        ['center', { 'margin-inline': 'auto' }],
+      ],
+      theme: {
+        colors: {
+          title: '#4a5a6a',
+          main: '#dddddd',
+        },
+        breakpoints: {
+          sm: '0px',
+          md: '650px',
+        },
+      },
+    }),
+  ],
   server: {
     port: 3000,
   },
