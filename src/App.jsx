eccf1e80edbb6757a6936477c8b298a68182189f
carmine_dyj <dyujie@outlook.com>
2023-01-11 23:58:52 +0800

    feat: password display WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
+import { createSignal } from 'solid-js'
+
 function App() {
+  const [password, setPassword] = createSignal('123!a@5678')
   return (
-    <>
-      <h1 class="text-title text-center text-18px font-extrabold center sm:mt-60px md:mt-100px sm:mb-18px md:mb-28px">
+    <div class="font-mono">
+      <h1 class="text-title text-center text-20px font-extrabold center sm:mt-60px md:mt-100px sm:mb-16px md:mb-32px">
         Solid Password Generator
       </h1>
-      <main class="main-bg-color center sm:w-85vw md:w-600px">
-        <div class="h-100px"></div>
+      <main class="main-bg-color center sm:w-85vw md:w-600px text-main">
+        <div class="h-80px ps-20px pe-20px flex flex-column items-center">
+          <div class="text-36px">{password}</div>
+        </div>
         <div class="index-bg-color h-30px"></div>
         <div class="h-400px"></div>
       </main>
-    </>
+    </div>
   )
 }
 
