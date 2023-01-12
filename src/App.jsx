b5c6e515f2e4dc02ad03e09dd2f88299b4072b10
carminedyj <dingyujie@ztgame.com>
2023-01-12 16:44:56 +0800

    refactor: refactor checkbox
    use solid For to render all password options

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import { createSignal } from 'solid-js'
+import { createSignal, For } from 'solid-js'
 import Checkbox from './components/Checkbox'
+
+const passwordInitOptions = {
+  uppercaseIncluded: false,
+  lowercaseIncluded: false,
+  numberIncluded: false,
+  signalIncluded: false,
+}
+const passwordOptionHints = [
+  'Include Uppercase Letters',
+  'Include Lowercase Letters',
+  'Include Numbers',
+  'Include Symbols',
+]
+const initPasswordOptions = () => {
+  Object.keys(passwordInitOptions).forEach((key) => {
+    passwordInitOptions[key] = localStorage.getItem(key) === 'true' ? true : false
+  })
+  return passwordInitOptions
+}
 
 function App() {
   const [password, setPassword] = createSignal('123!a@5678')
-  const [uppercaseIncluded, setUppercaseIncluded] = createSignal(false)
+  const [passwordOptions, setPasswordOptions] = createSignal(initPasswordOptions())
   return (
     <div class="font-mono text-18px">
       <h1 class="text-title text-center font-extrabold center sm:mt-60px md:mt-100px sm:mb-16px md:mb-32px">
@@ -19,14 +38,21 @@ function App() {
             <div class="text-18px">Character length</div>
             <div class="text-36px">{password().length}</div>
           </div>
-          <div class="flex items-center">
-            <Checkbox
-              class="sm:me-6px md:me-12px"
-              onClick={() => setUppercaseIncluded((prev) => !prev)}
-              uppercaseIncluded={uppercaseIncluded}
-            />
-            Include Uppercase Letters
-          </div>
+          <For each={Object.keys(passwordOptions())}>
+            {(option, i) => (
+              <div class="flex items-center mb-12px">
+                <Checkbox
+                  class="sm:me-6px md:me-12px"
+                  onClick={() => {
+                    setPasswordOptions({ ...passwordOptions(), [option]: !passwordOptions()[option] })
+                    localStorage.setItem(option, passwordOptions()[option])
+                  }}
+                  passwordOption={passwordOptions()[option]}
+                />
+                {passwordOptionHints[i()]}
+              </div>
+            )}
+          </For>
         </div>
       </main>
     </div>
--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -3,7 +3,7 @@ import { mergeProps } from 'solid-js'
 function Checkbox(props) {
   return (
     <div onClick={props.onClick} class={`checkbox ${props.class}`}>
-      <div class="checked" classList={{ invisible: !props.uppercaseIncluded() }}></div>
+      <div class="checked" classList={{ invisible: !props.passwordOption }}></div>
     </div>
   )
 }
