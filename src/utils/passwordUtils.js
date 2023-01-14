94f5242f4d788b4f6d9f368c32cf1b369a0c3b8d
carmine_dyj <dyujie@outlook.com>
2023-01-15 00:15:22 +0800

    fix(password options): options can back
    options can get back when reselect, rename file to passwordUtils

---
+++ src/utils/passwordUtils.js
@@ -0,0 +1,31 @@
+const patterns = {
+  lowercaseIncluded: '[a-z]',
+  uppercaseIncluded: '[A-Z]',
+  numberIncluded: '[0-9]',
+  signalIncluded: '[~@#$%^&_+\\-\\*/=.!?:;()]',
+}
+const strength = {
+  lowercaseIncluded: 0,
+  uppercaseIncluded: 0,
+  numberIncluded: 0,
+  signalIncluded: 0,
+}
+
+function passwordGenerate(options, length) {
+  let password = ''
+  while (password.length < length) {
+    const UTF16Char = String.fromCharCode(crypto.getRandomValues(new Uint8Array(1))[0])
+    // check if UTF16Char match all patterns in patterns
+    for (const key in patterns) {
+      if (!options[key]) continue
+      if (UTF16Char.match(patterns[key])) {
+        password += UTF16Char
+        strength[key] = 1
+      }
+    }
+  }
+  return { password }
+}
+
+export default passwordGenerate
+export { patterns }
